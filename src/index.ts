86142416ecf85f06fd6725cd9272b98134c6993c
oh-jinsu <ohjinsu98@icloud.com>
2025-04-11 16:40:39 +0900

    Add props

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 export type IML = {
   ch: number;
-  floor: [number, number, number][];
+  outerWalls: {
+    ps: [number, number];
+    pe: [number, number];
+    thickness: number;
+    height: number;
+  }[];
+  innerWalls: {
+    ps: [number, number];
+    pe: [number, number];
+    thickness: number;
+    height: number;
+  }[];
 };
